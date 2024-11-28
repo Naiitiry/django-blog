a86a9299762f5936250c8f79f53f0471477fd75d
Naiitiry <danchukroman@gmail.com>
2024-11-28 14:41:45 -0300

    login,register,logout

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('post/',include('post.urls')),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+@login_required
 def index(request):
     return render(request,'index.html',{})
+
+def register(request):
+    pass
+
+def login(request):
+    pass
